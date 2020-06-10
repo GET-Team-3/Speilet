1ec65009e6ce70494519fc452418e8d747b530d2
Vaughub <olakamfjord@gmail.com>
2020-06-10 12:24:50 +0200

    reg css

--- registrationView.js
+++ registrationView.js
@@ -1,10 +1,11 @@
 function registrationView() {
     document.getElementById('content').innerHTML = `
     <div id="parent" class="mainDiv">
-    <h3>Register</h3>
-    <input id="nameInput1" type="text" placeholder="First name" />
-    <input id="nameInput2" type="text" placeholder="Last name" />
-    <button onclick="registerNextPage()" id="registerForward">➡</button>
-  </div>
+    <h3 class="regH3">Register</h3>
+    <input id="nameInput1" type="text" class="regInput" placeholder="First name" oninput="model.user.name=this.value"/>
+    <input id="nameInput2" type="text" class="regInput" placeholder="Last name" oninput="model.user.contact=this.value"/>
+    <button class="regButton" onclick="registerNextPage()">➡</button>
+    </div>
     `;
 }
+
