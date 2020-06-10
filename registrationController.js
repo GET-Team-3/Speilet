c02f2a35b7be5e9c4b3a45fc61258ed0207d4a61
mrskme <mrskme@hotmail.no>
2020-06-10 12:15:03 +0200

    Name registration og next page

--- registrationController.js
+++ registrationController.js
@@ -1,10 +1,14 @@
-function firstNamePush(firstName) {
-    model.user.firstName = firstName;
-}
-function lastNamePush(lastName) {
-    model.user.lastName = lastName;
-}
-function nextPage() {
-    model.page = "surveyView"
+function registerNextPage() {
+    let firstNameValue = document.getElementById("nameInput1").value
+    let lastNameValue = document.getElementById("nameInput2").value
+    model.user.firstName += firstNameValue;
+    model.user.lastName += lastNameValue;
+    let letters = /^[A-Za-z ]+$/;
+
+    if (firstNameValue.match(letters) && lastNameValue.match(letters)) {
+    model.page = "surveyView";
     updateView();
-}
+    } else {
+        alert("Please enter letters from the alphabetic alphabet");
+    }
+    };
--- registrationView.js
+++ registrationView.js
@@ -2,9 +2,9 @@ function registrationView() {
     document.getElementById('content').innerHTML = `
     <div id="parent" class="mainDiv">
     <h3>Register</h3>
-    <input oninput="firstNamePush(this)" id="nameInput1" type="text" placeholder="First name" />
-    <input oninput="lastNamePush(this)" id="nameInput2" type="text" placeholder="Last name" />
-    <button onclick="nextPage()" id="registerForward">➡</button>
+    <input id="nameInput1" type="text" placeholder="First name" />
+    <input id="nameInput2" type="text" placeholder="Last name" />
+    <button onclick="registerNextPage()" id="registerForward">➡</button>
   </div>
     `;
 }
