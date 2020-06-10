8a5f4903af6094f6c8c0b1338d1c809db4d261f8
mrskme <mrskme@hotmail.no>
2020-06-10 12:33:04 +0200

    sdf

--- registrationController.js
+++ registrationController.js
@@ -1,11 +1,10 @@
 function registerNextPage() {
     let firstNameValue = document.getElementById("nameInput1").value
     let lastNameValue = document.getElementById("nameInput2").value
-    model.user.firstName += firstNameValue;
-    model.user.lastName += lastNameValue;
     let letters = /^[A-Za-z ]+$/;
-
     if (firstNameValue.match(letters) && lastNameValue.match(letters)) {
+    model.user.firstName = firstNameValue;
+    model.user.lastName = lastNameValue;
     model.page = "surveyView";
     updateView();
     } else {
