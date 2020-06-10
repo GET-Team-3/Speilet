b6543526b382c919670d3826a3e37c8fa67fca73
Vaughub <olakamfjord@gmail.com>
2020-06-10 14:09:59 +0200

    Update registrationController.js

--- registrationController.js
+++ registrationController.js
@@ -3,10 +3,10 @@ function registerNextPage() {
     let lastNameValue = document.getElementById("nameInput2").value
     let letters = /^[A-Za-z ]+$/;
     if (firstNameValue.match(letters) && lastNameValue.match(letters)) {
-    model.user.firstName = firstNameValue;
-    model.user.lastName = lastNameValue;
-    model.page = "surveyView";
-    updateView();
+        model.user.firstName = firstNameValue;
+        model.user.lastName = lastNameValue;
+        model.page = "surveyView";
+        updateView();
     } else {
         alert("Please enter letters from the alphabetic alphabet");
     }
