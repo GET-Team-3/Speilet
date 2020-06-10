d270e93ca4267745bc35caf9f75bc90c48248d42
Vaughub <olakamfjord@gmail.com>
2020-06-10 14:42:08 +0200

    Update registrationView.js

--- registrationView.js
+++ registrationView.js
@@ -1,11 +1,11 @@
 function registrationView() {
     document.getElementById('content').innerHTML = `
-    <div id="parent" class="regMainDiv">
-    <h3 class="regH3">Register</h3>
-    <input class="regInputs" id="nameInput1" type="text" placeholder="First name" />
-    <input class="regInputs" id="nameInput2" type="text" placeholder="Last name" />
-    <button class="regButton" onclick="registerNextPage()" id="registerForward">➡</button>
-  </div>
+        <div id="parent" class="regMainDiv">
+        <h3 class="regH3">Register</h3>
+        <input class="regInputs" id="nameInput1" type="text" placeholder="First name" />
+        <input class="regInputs" id="nameInput2" type="text" placeholder="Last name" />
+        <button class="regButton" onclick="registerNextPage()" id="registerForward">➡</button>
+        </div>
     `;
 }
 
