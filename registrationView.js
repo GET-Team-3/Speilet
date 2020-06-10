6f40bbfe17dc06705f3aacd31b82655c58b2c87c
Vaughub <olakamfjord@gmail.com>
2020-06-10 11:15:44 +0200

    small error

--- registrationView.js
+++ registrationView.js
@@ -2,8 +2,8 @@ function registrationView() {
     document.getElementById('content').innerHTML = `
         <div id="parent" class="mainDiv">
         <h3 class="regH3">Register</h3>
-        <input id="nameInput" type="text" class="regInput" placeholder="First name oninput="model.user.name=this.value"/>
-        <input id="nameInput" type="text" class="regInput" placeholder="Last name oninput="model.user.contact=this.value""/>
+        <input id="nameInput" type="text" class="regInput" placeholder="First name" oninput="model.user.name=this.value"/>
+        <input id="nameInput" type="text" class="regInput" placeholder="Last name" oninput="model.user.contact=this.value"/>
         <button class="regButton">➡</button>
         </div>
     `;
