ba65eda2d2a847355da635257f5dc89a0016b4aa
Vaughub <olakamfjord@gmail.com>
2020-06-16 14:36:43 +0200

    Update surveyView.js

--- surveyView.js
+++ surveyView.js
@@ -3,7 +3,7 @@ function surveyView() {
     <div class="page">
             <p onclick="tempBackwardButton()" class="bV" style="${model.questionCounter == 0 ? 'color:lightgray;cursor: not-allowed;' : ''}">◀</p>
         <div class="progressBar">
-            <div class="progressBarInside" id="bar" style="width: ${model.progressBar < 99 ? model.progressBar : 100}%;opacity: ${model.questionCounter == 0 ? 0 : 1}"></div>
+            <div class="progressBarInside" id="bar" style="width: ${model.progressBar < 95 ? model.progressBar : 100}%;opacity: ${model.questionCounter == 0 ? 0 : 1}"></div>
         </div>
         <div class="survey">
             <h2 class="theme">${model.theme[model.themeCounter]}</h2>
