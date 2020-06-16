0fd0851701febd6d8fb08b7a03191ff1c01d4611
Vaughub <olakamfjord@gmail.com>
2020-06-16 14:33:07 +0200

    Update surveyController.js

--- surveyController.js
+++ surveyController.js
@@ -1,25 +1,20 @@
 function tempBackwardButton() {
     console.log(model.questionCounter)
-    let stg = model.questionCounter.toString()
     if (model.questionCounter == 0) return;
-    if (stg.match(/^4|8|12|16|20$/)) model.themeCounter--;
+    if ([4,8,12,16,20].includes(model.questionCounter)) model.themeCounter++;
     model.questionCounter--;
     model.progressBar -= 4.16666666666;
     updateView();
 }
 
 function tempForwardButton() {
-    // console.log(model.questionCounter)
     let stg = model.questionCounter.toString()
     if (radioCheck()) {
         if (model.questionCounter == 23) {
             // model.page = 'resultView';
             return updateView();
         }
-        if (stg.match(/^3|7|11|15|19$/)) {
-            console.log(model.questionCounter)
-            model.themeCounter++;
-        }
+        if ([3,7,11,15,19].includes(model.questionCounter)) model.themeCounter++;
         model.questionCounter++;
         model.progressBar += 4.16666666666;
         updateView();
