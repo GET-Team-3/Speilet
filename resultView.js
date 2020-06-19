07a659490fd4880f94bc3c21c649f31dd466c2d9
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-06-19 14:41:32 +0200

    Design, now lets make it functional

--- resultView.js
+++ resultView.js
@@ -3,31 +3,43 @@ resultView();
 function resultView() {
 console.log(model.user.data);
         document.getElementById('content').innerHTML = `
-        <text class="kategori">Kategori</text>   
-        <svg class="chart" viewBox="0 0 100 100" height="300" width="300">
-            <g class="bar1">
-                <text x="0" y="7">Alle</text>
-                <rect x="0" y="10" width="80" height="90"></rect>
-              </g> 
-        
-          <g class="bar">
-            <text x="0" y="7">Samhold</text>
-            <rect x="0" y="50" width="20" height="50"></rect>
-          </g>
-        
-          <g class="bar">
-            <text x="0" y="7">Produksjon</text>
-            <rect x="20" y="80" width="20" height="20"></rect>
-          </g>
-        
-          <g class="bar">
-            <text x="0" y="7">Drift</text>
-            <rect x="40" y="20" width="20" height="80"></rect>
-          </g>
-        
-          <g class="bar">
-            <text x="0" y="7">Lederskap</text>  
-            <rect x="60" y="40" width="20" height="60"></rect>
-        </g>
-        </svg>
+        <div class="divContainer">
+<text class="kategori">Kategori</text> 
+<div class="points">
+  <div class="pointa">7</div>
+  <div class="pointa">6</div>
+  <div class="pointa">5</div>
+  <div class="pointa">4</div>
+  <div class="pointa">3</div>
+  <div class="pointa">2</div>
+  <div class="pointa">1</div>
+</div>
+<svg class="chart" viewBox="0 0 8 8" height="300" width="600">
+  <g class="bar1">
+      <rect class="hovercolor" x="-4" y="4" width="16" height="4"></rect>
+      <text x="-4" y="4">Alle</text>
+  </g> 
+
+  <g class="bar">
+    <rect class="hovercolor" x="-4" y="7" width="4" height="1"></rect>
+    <text x="-4" y="7">Samhold</text>
+  </g>
+
+  <g class="bar">
+    <rect class="hovercolor" x="0" y="5" width="4" height="3"></rect>
+    <text x="0" y="5">Produksjon</text>
+  </g>
+
+  <g class="bar">
+    <rect class="hovercolor" x="4" y="3" width="4" height="5"></rect>
+    <text x="4" y="3">Drift</text>
+  </g>
+
+  <g class="bar">
+    <rect class="hovercolor" x="8" y="1" width="4" height="7"></rect>
+    <text x="8" y="1">Lederskap</text>
+</g>
+</svg>
+<div class="explain"></div>
+</div>
         `;}
