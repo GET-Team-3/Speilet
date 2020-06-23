a10f273872642cb712f406127db98b11526ba291
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-06-23 11:00:53 +0200

    vovovo

--- resultController.js
+++ resultController.js
@@ -1,12 +1,12 @@
 
 
-function theTheme() {
-    i=0; i<model.theme.length; i++;
-    return model.theme[i];
+function theScore() {
+    for (i = 0; i < model.user.answer.length; i++) {
+        model.user.answer[i];
  }   
 
 
-function theScore() {
+function the() {
     for(i=0; i<model.user.answer.length; i++)
     return model.user.answer[i];
 }
--- resultView.js
+++ resultView.js
@@ -1,10 +1,9 @@
 function resultView() {
-  console.log(model.theme)
+  console.log()
 let html = '';
 for (i = 0; i < 6; i++) {
 html += `
 <div class="divContainer">
-<br>
 <text class="kategori">${model.theme[i]}</text> 
 <div class="points">
   <div class="pointa">7</div>
@@ -20,53 +19,33 @@ html += `
       <rect class="hovercolor" x="-4" y="4" width="16" height="4"></rect>
       <text x="-4" y="4">Alle</text>
   </g> 
+
+  <g class="bar">
+    <rect class="hovercolor" x="-4" y="${(8-model.user.answer[i])}" width="4" height="${model.user.answer[i]}"></rect>
+    <text x="-4" y="4">4</text>
+  </g>
+
+  <g class="bar">
+    <rect class="hovercolor" x="0" y="${(8-model.user.answer[i])}" width="4" height="${model.user.answer[i]}"></rect>
+    <text x="0" y="4">4</text>
+  </g>
+
+  <g class="bar">
+    <rect class="hovercolor" x="4" y="${(8-model.user.answer[i])}" width="4" height="${model.user.answer[i]}"></rect>
+    <text x="4" y="4">4</text>
+  </g>
+
+  <g class="bar">
+    <rect class="hovercolor" x="8" y="${(8-model.user.answer[i])}" width="4" height="${model.user.answer[i]}"></rect>
+    <text x="8" y="4">4</text>
+</g>
+</svg>
+<div class="explain">questionheader</div>
+</div>
   `;
 }
 document.getElementById('content').innerHTML = html;
 }
 
-// function resultView() {
-//   console.log(model.theme)
-// document.getElementById('content').innerHTML = `
-// <div class="divContainer">
-// <text class="kategori">${theTheme()}</text> 
-// <div class="points">
-//   <div class="pointa">7</div>
-//   <div class="pointa">6</div>
-//   <div class="pointa">5</div>
-//   <div class="pointa">4</div>
-//   <div class="pointa">3</div>
-//   <div class="pointa">2</div>
-//   <div class="pointa">1</div>
-// </div>
-// <svg class="chart" viewBox="0 0 8 8" height="300" width="600">
-//   <g class="bar1">
-//       <rect class="hovercolor" x="-4" y="4" width="16" height="4"></rect>
-//       <text x="-4" y="4">Alle</text>
-//   </g> 
 
-//   <g class="bar">
-//     <rect class="hovercolor" x="-4" y="${8-theScore()}" width="4" height="${theScore()}"></rect>
-//     <text x="-4" y="${8-theScore()}">${theScore()}</text>
-//   </g>
-
-//   <g class="bar">
-//     <rect class="hovercolor" x="0" y="${8-theScore()}" width="4" height="${theScore()}"></rect>
-//     <text x="0" y="${8-theScore()}">${theScore()}</text>
-//   </g>
-
-//   <g class="bar">
-//     <rect class="hovercolor" x="4" y="${8-theScore()}" width="4" height="${theScore()}"></rect>
-//     <text x="4" y="${8-theScore()}">${theScore()}</text>
-//   </g>
-
-//   <g class="bar">
-//     <rect class="hovercolor" x="8" y="${8-theScore()}" width="4" height="${theScore()}"></rect>
-//     <text x="8" y="${8-theScore()}">${theScore()}</text>
-// </g>
-// </svg>
-// <div class="explain">${questionHeader()}</div>
-// </div>
-// <text class="kategori">${theTheme()}</text> 
-//         `;}
 
