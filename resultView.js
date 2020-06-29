0314606d540456b17353fdc28444244b706c1397
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-06-29 19:50:18 +0200

    nå skjer det ting vett

--- resultView.js
+++ resultView.js
@@ -1,3 +1,5 @@
+
+
 function resultView() {
   let html = '';
   for (i = 0; i < 6; i++) {
@@ -14,53 +16,66 @@ function resultView() {
         <div class="pointa">2</div>
         <div class="pointa">1</div>
     </div>
-    <svg class="chart" viewBox="0 0 7 7" height="300" width="300">
+    <svg class="chart" viewBox="0 0 7 7" height="300" width="360">
     
-    <rect class="scale1" x="0" y="5" width="0.2" height="2"></rect>
-    <rect class="scale2" x="0" y="2" width="0.2" height="3"></rect>
-    <rect class="scale3" x="0" y="0" width="0.2" height="2"></rect>
+    <rect class="scale1" x="-0.6" y="5" width="0.2" height="2"></rect>
+    <rect class="scale2" x="-0.6" y="2" width="0.2" height="3"></rect>
+    <rect class="scale3" x="-0.6" y="0" width="0.2" height="2"></rect>
 
         ${theMid(i)}
         
         ${tempo(i)}
     </svg>
-    <div class="explain">questionheader</div>
+    <div class="explain">Text</div>
     </div>
     `;
 
+  function theMid(indexo) {
+    let html = '';
+    for (oi = 0; oi < 1; oi++) {
+      var verdi = (model.user.answer[0+(i*4)]+model.user.answer[1+(i*4)]+model.user.answer[2+(i*4)]+model.user.answer[3+(i*4)]);
+      var lengde = (verdi / 4);
+        html += `
+          <g class="bar1">
+            <rect class="hovercolor" x="-0.4" y="${7-lengde}" width="8" height="${lengde}"></rect>
+            <text x="-4" y="${(7-lengde)}">${lengde}</text>
+          </g>
+        `;
+    }
+    
+    return html;
+    
   }
-  document.getElementById('content').innerHTML = html;
-}
-
-function tempo(index) {
+  }
+  
+  function tempo(index) {
+  let color = 'fill:rgb(168, 228, 163)';
   let html = '';
   for (y = 0; y < 4; y++) {
+    let answer = model.user.answer[y + (index * 4)];
+    if (answer < 5) {color = 'fill:rgb(253, 249, 160)'}
+    if (answer < 2) {color = 'fill:rgb(255, 111, 108)'}
       html += `
-          <g class="bar" id="barsa">
-              <rect class="hovercolor" x="${1 + (1 * y)}" y="${(7-model.user.answer[y + (index * 4)])}" width="1" height="${model.user.answer[y + (index * 4)]}"></rect>
-              <text x="${1 + (1 * y)}" y="${(7-model.user.answer[y + (index * 4)])}">${model.user.answer[y + (index * 4)]}</text>
+          <g class="bar" style="${color}" onmouseover="${mouseOver()}">
+              <rect class="hovercolor" x="${-0.4 + (2 * y)}" y="${(7-answer)}" width="2" height="${answer}" ></rect>
+              <text x="${0.3 + (2 * y)}" y="${(8-answer)}">${answer}</text>
           </g>
       `;
       
   }
   return html;
+  
+}
+  document.getElementById('content').innerHTML = html;
 }
 
-function theMid(indexo) {
-  let html = '';
-  for (y = 0; y < 1; y++) {
-    var verdi = (model.user.answer[0]+model.user.answer[1]+model.user.answer[2]+model.user.answer[3]);
-    var lengde = (verdi / 4);
-      html += `
-        <g class="bar1">
-          <rect class="hovercolor" x="1" y="${7-lengde}" width="4" height="${lengde}"></rect>
-          <text x="-4" y="${(7-lengde)}">${lengde}</text>
-        </g>
-      `;
-  }
-  console.log(verdi);
-  return html;
+function mouseOver() {
+  theQuestion = document.getElementsByClassName('explain');
+  x= theQuestion.innerHTML;
+  x += model.survey[0].question;
 }
+
+
 
 
   
