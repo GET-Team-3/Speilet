fd59652e4edf375d6d60d1acf2c6b370f4ed1dcd
mrskme <mrskme@hotmail.no>
2020-07-01 16:51:39 +0200

    Update model.js

--- model.js
+++ model.js
@@ -9,7 +9,7 @@ const model = {
     },
     userId: '',
     session: '',
-    page: 'resultView2', //registrationView surveyView resultView1 resultView2
+    page: 'surveyView', //registrationView surveyView resultView1 resultView2
     themeCounter: 0,
     questionCounter: 0,
     progressBar: 0,
