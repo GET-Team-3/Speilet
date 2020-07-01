b2fdc864a7aa949d1558f979d5408a090c7959aa
mrskme <mrskme@hotmail.no>
2020-07-01 16:29:55 +0200

    azdsfasdfas

--- resultController.js
+++ resultController.js
@@ -25,9 +25,19 @@ function changeColors(index) {
         else if(bar < 6) array.push('rgb(255, 255, 92)');
         else if(bar < 8) array.push('rgb(53, 170, 53)');
     }
-    console.log(array)
     return array;
     
+}
+function alternativtResultat() {
+    if (model.page == "resultView1") {
+        model.page = "resultView2";
+        resultView2();
+    }
+    if (model.page == "resultView2") {
+        model.page = "resultView1";
+        resultView1();
+    }
+    console.log(model.page)
 }
 // function plotQuestion() {
 //     let array = [];
